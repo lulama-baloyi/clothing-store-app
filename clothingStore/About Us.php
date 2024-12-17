50ede32bec9c672ea92356141408039c1af39f16
lulama-baloyi <145808355+lulama-baloyi@users.noreply.github.com>
2024-12-17 14:56:05 +0000

    Add files via upload
    Initial commit for Clothing Store Project

---
+++ clothingStore/About Us.php
@@ -0,0 +1,98 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE-edge">
+<meta  name="viewport" content="width=device-width, initial-scale=1.0">
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script> 
+<title>About Us</title>
+
+<!--font awesome cdn link -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+<!-- custom css file link -->
+<link rel="stylesheet" href="css/sty.css">
+
+</head>
+<body>
+
+<section class="header" style=" background:url(images/20.png); background-repeat: no-repeat;   min-height: 60vh; background-position: center; background-size: cover">
+                       <nav>
+
+                <h3>LSS</h3>
+            
+<div class="nav-links">
+<ul>
+<li><a href="home.php" >Home Page</a></li>
+<li><a href="Products.php">Products </a></li>
+<li><a href="About Us.php" >About Us</a></li>
+<li><a href="Contact Us.php" >Contact Us</a> </li>
+<li><a href="Testimonials.php">Testimonials</a></li>
+	<li><a href="Cart.php">Cart</a></li>
+</ul>
+</div>
+</nav>
+
+    <div class="text-box">
+
+
+
+<p style="color:white"> About Us, Fast, Great and Trusted Delivery Service.
+</p>
+</div>
+</section>
+
+
+      <section class="About">
+
+
+          <h2><span class="ab">Deli</span>very</h2>
+<p> We Serve The People</p>
+      
+          <div class="row">
+ 
+   <div class="col">
+<p>
+    - Customization: Depending on  discounts model, <br />
+  Monthly basis we will have specials on takkies and clothing<br />
+    Ensure to have your money "racked up" <br />
+     limited selection.<br />
+   - Competitive : We aim to compete and surpass local markets,<br />
+    offering affordable prices could be a key factor in attracting customers.<br />
+    <br /><br /><br /><br /><br />
+    Overall, your clothing spree has the potential to provide a valuable<br />
+    service to students and adults by making it easier and more convenient for them to<br />
+    get the clothing they want, when they want it. With careful planning and execution,<br />
+    you could build a successful and thriving business.
+</p>
+
+ </div>
+
+               <div class="col">
+                    <br /><br /><br /><br /><br />
+<video width="550"  controls>
+    <source src="images/LULAMA1.mp4" type="video/mp4" />
+</video>
+
+ </div>
+ </div>
+          
+    </section>
+	
+	
+  <section class="footer">
+
+<br />
+          <br /><h2><span class="ab">LSS</span> Network</h2>
+<p> Order Now on LSS!</p>
+
+<span style="color:darkblue"><ion-icon name="logo-facebook"></ion-icon></span>
+  <span style="color:deeppink">  <ion-icon name="logo-instagram"></ion-icon></span>
+     <span style="color:lightblue">   <ion-icon name="logo-twitter"></ion-icon></span>
+
+
+      </section>
+</body>
+</html>
---
+++ clothingStore/home.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE-edge">
+<meta  name="viewport" content="width=device-width, initial-scale=1.0">
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script> 
+<title>Home</title>
+
+<!--font awesome cdn link -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+<!-- custom css file link -->
+<link rel="stylesheet" href="css/sty.css">
+
+</head>
+<body>
+<section class="header" style=" background:url(images/12.jpg); background-repeat: no-repeat; background-position: center; background-size: cover">
+            <nav>
+
+                <h3>LSS</h3>
+            
+<div class="nav-links">
+<ul>
+<li><a href="home.php" >Home Page</a></li>
+<li><a href="Products.php">Products </a></li>
+<li><a href="About Us.php" >About Us</a></li>
+<li><a href="Contact Us.php" >Contact Us</a> </li>
+<li><a href="Testimonials.php">Testimonials</a></li>
+	<li><a href="Cart.php">Cart</a></li>
+</ul>
+</div>
+</nav>
+
+    <div class="text-box">
+
+<h2>Lulama Shopping Spree</h2>
+
+<p>BEST CLOTHING COMPANY.
+</p>
+
+<a href=" About Us.php"  class="hero-btn">Visit Us To know More</a> 
+</div>
+</section>
+
+      <section class="About">
+
+<br />
+          <br /><h2><span class="ab">About</span> us</h2>
+<p> We Serve The People</p>
+      
+          <div class="row">
+ <div class="col">
+<video width="550" controls>
+    <source src="images/LULAMA.MOV" type="video/mp4" />
+</video>
+
+ </div>
+   <div class="col">
+<p>
+    In the bustling streets of Johannesburg, Lulama embarked on a shopping spree like no other. Armed with her credit card and an insatiable desire for style, she set out to conquer the fashion world in 2024.<br />
+    Soweto’s street market beckoned with its chaotic charm. Lulama haggled with a vendor over a beaded Zulu bracelet <br />
+    The sun-kissed beads seemed to hold centuries of wisdom<br />
+As the sun dipped below the Magaliesberg mountains, Lulama found herself at Sandton City Mall. <br />
+    She splurged on designer sunglasses, sipping champagne at a rooftop bar.<br />
+The city lights twinkled below, and Lulama thought, <br />
+   This is my South African symphony—a blend of tradition, modernity, and dreams.<br />
+
+</p>
+
+ </div>
+ </div>
+          
+    </section>
+          <section class="footer">
+
+<br />
+          <br /><h2><span class="ab">LMS</span> Network</h2>
+<p> Order Now!</p>
+
+<span style="color:darkblue"><ion-icon name="logo-facebook"></ion-icon></span>
+  <span style="color:deeppink">  <ion-icon name="logo-instagram"></ion-icon></span>
+     <span style="color:lightblue">   <ion-icon name="logo-twitter"></ion-icon></span>
+
+
+      </section>
+</body>
+</html>
